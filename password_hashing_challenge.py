f066a30623ead57a5a5258a8940e4502e8b9625f
ReedWatkins <reedwatkins9794@yahoo.com>
2019-04-04 11:21:21 -0500

    finished hashing challenge

--- password_hashing_challenge.py
+++ password_hashing_challenge.py
@@ -3,8 +3,7 @@ import hashlib
 import hmac
 import base64
 from binascii import hexlify
-#import scrypt
-from pylibscrypt import scrypt
+import scrypt
 import requests
 from json import dumps
 
@@ -22,16 +21,11 @@ hmac256 = hmac.new(decodedSalt, password, hashlib.sha256).hexdigest()
 
 pbkdf2 = hexlify(hashlib.pbkdf2_hmac(j['pbkdf2']['hash'], password, decodedSalt, j['pbkdf2']['rounds'])).decode('utf-8')
 
-'''
-		Need to fix the scrypt part of this
-'''
 N = j['scrypt']['N']
 r = j['scrypt']['r']
 p = j['scrypt']['p']
 buflen = j['scrypt']['buflen']
-# scrypt = hexlify(scrypt.hash(password, salt, N, r, p, buflen)).decode('utf-8')
-#scrypt = hexlify(scrypt(password, salt, N, r, p, buflen)).decode('utf-8')
-scrypt = input("Scrypt: ")
+scrypt = hexlify(scrypt.hash(password, salt, N, r, p, buflen)).decode('utf-8')
 
 print(f"Base SHA-256: {sha256}")
 print(f"HMAC-256: {hmac256}")
